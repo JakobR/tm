9fbd0ccd524b4ca4b90953ad94d88a91f5898bcf
Jakob Rath <git@jakobrath.eu>
2018-10-26 13:07:07 +0200

    Nicer output, run multiple simulations

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
+
+-- base
+import Control.Monad ( forM_, mapM_ )
+
+-- ansi-terminal
+import System.Console.ANSI (
+  setSGR, SGR(..), ConsoleLayer(..), Color(..), ColorIntensity(..), ConsoleIntensity(..), Underlining(..)
+  )
 
 -- containers
 import Data.Map ( Map )
@@ -9,8 +19,13 @@ import qualified Data.Map as Map
 import Data.Set ( Set )
 import qualified Data.Set as Set
 
+-- mtl
+import Control.Monad.Writer ( MonadWriter(..), runWriter )
+import Control.Monad.Except ( MonadError(..), runExceptT )
+
 -- text
 import Data.Text ( Text )
+import qualified Data.Text as Text
 
 -- safe
 import Safe ( headDef, tailSafe )
@@ -39,9 +54,9 @@ data Configuration state symbol = Configuration
   , cfState :: state         -- ^ current state
   , cfRightTape :: [symbol]  -- ^ what's to the right of the reading head (reading head points to first symbol of cfRightTape)
   }
-  deriving (Eq, Ord)
+  deriving (Eq, Ord, Show)
 
-tmBJ :: TM Text Char
+tmBJ :: TM State Symbol
 tmBJ = TM{..}
   where
     tmInitialState = "q1"
@@ -77,16 +92,19 @@ step TM{..} Configuration{..} = do
   return $ case movement of
     L -> Configuration { cfLeftTape = tailSafe cfLeftTape
                        , cfState = nextState
-                       , cfRightTape = trimBlanks3 tmBlankSymbol $ headDef tmBlankSymbol cfLeftTape : writeSymbol : tailSafe cfRightTape
+                       , cfRightTape = trimBlanks $ headDef tmBlankSymbol cfLeftTape : writeSymbol : tailSafe cfRightTape
                        }
     S -> Configuration { cfLeftTape = cfLeftTape
                        , cfState = nextState
-                       , cfRightTape = trimBlanks3 tmBlankSymbol $ writeSymbol : tailSafe cfRightTape
+                       , cfRightTape = trimBlanks $ writeSymbol : tailSafe cfRightTape
                        }
-    R -> Configuration { cfLeftTape = trimBlanks3 tmBlankSymbol $ writeSymbol : cfLeftTape
+    R -> Configuration { cfLeftTape = trimBlanks $ writeSymbol : cfLeftTape
                        , cfState = nextState
                        , cfRightTape = tailSafe cfRightTape
                        }
+  where
+    -- trimBlanks = trimBlanks3 tmBlankSymbol
+    trimBlanks = id  -- NOTE: uncomment this to disable blank trimming
 
 -- | removes unneeded blank symbols at the border of the tape (only checks three symbols, which should be enough when it's done after every step)
 trimBlanks3 :: Eq symbol => symbol -> [symbol] -> [symbol]
@@ -105,33 +123,90 @@ data SimulationResult -- state symbol
   | Timeout -- (Configuration state symbol)
   deriving (Show)
 
-simulate :: (Ord state, Ord symbol) => Int -> TM state symbol -> [symbol] -> SimulationResult
+simulate :: (Ord state, Ord symbol) => Int -> TM state symbol -> [symbol] -> (SimulationResult, [Configuration state symbol])
 simulate maxSteps tm@TM{..} inputWord =
   let initialCf = Configuration { cfLeftTape = []
                                 , cfState = tmInitialState
                                 , cfRightTape = inputWord
                                 }
-  in case simulate' maxSteps mempty tm initialCf of
-    Left _ -> Timeout
-    Right result -> result
+      (result', trace) = runWriter $ runExceptT $ simulate' maxSteps mempty tm initialCf
+      result = case result' of
+        Left _ -> Timeout
+        Right Accept' -> Accept
+        Right Reject' -> Reject
+        Right Loop' -> Loop
+  in (result, trace)
 
--- Left means timeout, Right means termination (or loop detected)
-simulate' :: (Ord state, Ord symbol)
+data SimulationResult'
+  = Accept'
+  | Reject'
+  | Loop'
+  deriving Show
+
+-- Error means timeout here.
+-- The writer accumulates a trace of configurations.
+simulate' :: (Ord state, Ord symbol, MonadWriter [Configuration state symbol] m, MonadError (Configuration state symbol) m)
           => Int
           -> Set (Configuration state symbol)
           -> TM state symbol
           -> Configuration state symbol
-          -> Either (Configuration state symbol) SimulationResult
-simulate' 0 _ _ cf = Left cf
-simulate' maxSteps seenCfs tm@TM{..} cf@Configuration{..} =
+          -> m SimulationResult'
+simulate' 0 _ _ cf = throwError cf
+simulate' maxSteps seenCfs tm@TM{..} cf@Configuration{..} = do
+  tell [cf]
   case step tm cf of
-    Nothing -> return $ if cfState == tmFinalState then Accept else Reject
+    Nothing -> return $ if cfState == tmFinalState then Accept' else Reject'
     Just newCf ->
       if newCf `Set.member` seenCfs
-      then return Loop
+      then return Loop'
       else simulate' (maxSteps - 1) (newCf `Set.insert` seenCfs) tm newCf
 
 
+printConfiguration :: TM State Symbol -> Configuration State Symbol -> IO ()
+printConfiguration TM{..} Configuration{..} = do
+  putStr $ Text.unpack cfState
+  putStr ":\t"
+  putStr cfLeftTape
+  setSGR [ SetUnderlining SingleUnderline
+         , SetConsoleIntensity BoldIntensity
+         , SetColor Foreground Dull Yellow ]
+  putChar (headDef tmBlankSymbol cfRightTape)
+  setSGR []
+  putStr (tailSafe cfRightTape)
+  putChar '\n'
+
+
+runSimulation :: TM State Symbol -> [Symbol] -> Bool -> IO ()
+runSimulation tm input shouldAccept = do
+  let (result, trace) = simulate 10000 tm input
+  case result of
+    Accept -> if shouldAccept
+              then putStrLn $ "Success: \t" <> input
+              else do putStrLn $ "Error (accepted but should not accept): " <> input
+                      mapM_ (printConfiguration tm) trace
+                      putChar '\n'
+    Timeout -> putStrLn $ "Timeout: " <> input
+    _ -> if shouldAccept
+         then do putStrLn $ "Error (" <> show result <> " but should accept): " <> input
+                 mapM_ (printConfiguration tm) trace
+                 putChar '\n'
+         else putStrLn $ "Success: \t" <> input
+
+testTM :: TM State Symbol -> IO ()
+testTM tm =
+  forM_ [1..16] $ \(i :: Int) -> do
+    let word = replicate i 'a'
+    runSimulation tm word (i `elem` [ 2^n | (n :: Int) <- [0..10] ])
+
 main :: IO ()
 main = do
-  print $ simulate 10000 tmBJ "aabaaa"
+  -- runSimulation tmBJ "a" True
+  testTM tmBJ
+  -- let initialCf = Configuration { cfLeftTape = []
+  --                               , cfState = tmInitialState tmBJ
+  --                               , cfRightTape = "a"
+  --                               }
+  -- let (result, trace) = runWriter $ runExceptT $ simulate' 10000 mempty tmBJ initialCf
+  -- forM_ trace $ \cf ->
+  --   printConfiguration tmBJ cf
+  -- print result
